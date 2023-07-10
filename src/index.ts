d38d8ea2baa8a6e4903d7ef39e61891ba51c4179
VilsonSouza <vilsonsouza758@gmail.com>
2023-07-10 15:38:34 -0300

    chore: install typeorm

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+
+import dotenv from 'dotenv'
+import express from 'express'
+
+dotenv.config()
+
+const PORT = process.env.PORT || 8080
+
+const app = express()
+app.get('/', (request,response)=>{
+  response.send("Server Up")
+})
+app.listen(PORT, () =>{
+  console.log(`Server Running in port ${PORT}`)
+})
