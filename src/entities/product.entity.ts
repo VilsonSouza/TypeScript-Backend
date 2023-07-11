0d00a8ddb36d81ccf4c15c5d770ea7460f7882cf
VilsonSouza <vilsonsouza758@gmail.com>
2023-07-11 10:50:07 -0300

    chore: import uuid

--- src/entities/product.entity.ts
+++ src/entities/product.entity.ts
@@ -1,5 +1,5 @@
 import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm"
-
+import {v4 as uuid} from 'uuid'
 @Entity('products')
 export class Product {
   @PrimaryColumn()
@@ -16,5 +16,11 @@ export class Product {
     type: 'timestamp'
   })
   createdAt:Date;
+
+  constructor(){
+    if(!this.id){
+      this.id = uuid()
+    }
+  }
 
 }
